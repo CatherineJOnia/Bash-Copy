ccb11b2973a2bf6784a177b1fe22a4b5dbf62439
rachelcorawood <rachelcorawood@gmail.com>
2022-11-10 13:01:41 -0600

    cora's additions

---
+++ pwd.js
@@ -0,0 +1,25 @@
+const { cwd } = require('node:process');
+// console.log(`Current directory: ${cwd()}`);
+
+//Output a prompt
+process.stdout.write('prompt > ');
+
+
+module.exports = process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); //remove the newline
+  process.stdout.write('You typed: ' + cmd);
+if(cmd === 'pwd') {
+  process.stdout.write(`\n${cwd()}`);
+}
+  process.stdout.write('\nprompt > ');
+});
+
+// The stdin 'data' event fires after a user types in a line
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim(); //remove the newline
+//   process.stdout.write('You typed: ' + cmd);
+// if(cmd === 'pwd') {
+//   process.stdout.write(`\n${cwd()}`);
+// }
+//   process.stdout.write('\nprompt > ');
+// });
